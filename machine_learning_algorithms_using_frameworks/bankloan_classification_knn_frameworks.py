ecabb7ea4d691deba77ceed4509db5074fea1c4a
karthik <karthikkannan@gmail.com>
2020-06-28 17:33:15 +0530

    new files

--- machine_learning_algorithms_using_frameworks/bankloan_classification_knn_frameworks.py
+++ machine_learning_algorithms_using_frameworks/bankloan_classification_knn_frameworks.py
@@ -1,26 +1,21 @@
 # KNN Classification
 from pandas import read_csv
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
 filename = '../datasets/bankloan_classification_train.csv'
 names = ['age', 'loanamount', 'status']
-dataframe = read_csv(filename, names=names)
-array = dataframe.values
-X = array[:,0:2]
-Y = array[:,2]
-model = LogisticRegression(solver="liblinear")
-print(model.fit(X,Y))
+df = read_csv(filename, names=names)
+array = df.values
+inputx = array[:,0:2]
+outputy = array[:,2]
+thismodel = KNeighborsClassifier()
+print(thismodel.fit(inputx,outputy))
 filename = '../datasets/bankloan_classification_test.csv'
 names = ['age', 'loanamount']
 newdataframe = read_csv(filename, names=names)
 array = newdataframe.values
-z = array[0:4:,0:2]
+testinputz = array[0:4,0:2]
 print(newdataframe)
-res=model.predict(z)
+res=thismodel.predict(testinputz)
 reslist=[]
 for val in res:
     if val==0:
@@ -28,4 +23,5 @@ for val in res:
     else:
         reslist.append("WillPay")
 print(reslist)
-print(model.predict(z))
+res=thismodel.predict(testinputz)
+print(thismodel.predict(testinputz))
