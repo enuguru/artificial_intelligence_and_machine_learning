0ded846cec7bed8dafe881be97cf18205da898dd
karthik <karthikkannan@gmail.com>
2020-12-01 21:49:09 +0530

    all files

--- Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/multiple_linear_regression.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/multiple_linear_regression.py
@@ -29,5 +29,6 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+print("see this",y_pred)
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
