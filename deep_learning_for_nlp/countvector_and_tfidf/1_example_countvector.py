cf1bed9758b3e28e0055073a3139739c9c9f21bd
karthik <karthikkannan@gmail.com>
2020-12-18 09:58:40 +0530

    all files

---
+++ deep_learning_for_nlp/countvector_and_tfidf/1_example_countvector.py
@@ -0,0 +1,15 @@
+from sklearn.feature_extraction.text import CountVectorizer
+# list of text documents
+text = ["The quick brown fox jumped over the lazy dog."]
+# create the transform
+vectorizer = CountVectorizer()
+# tokenize and build vocab
+vectorizer.fit(text)
+# summarize
+print(vectorizer.vocabulary_)
+# encode document
+vector = vectorizer.transform(text)
+# summarize encoded vector
+print(vector.shape)
+print(type(vector))
+print(vector.toarray())
